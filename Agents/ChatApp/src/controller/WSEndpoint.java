adf264ac5d18e7d104e7983a7025c48e058ff6c5
Zlatan <zlajox@gmail.com>
2017-06-15 13:16:24 +0200

    WS update Type and running agents

--- Agents/ChatApp/src/controller/WSEndpoint.java
+++ Agents/ChatApp/src/controller/WSEndpoint.java
@@ -1,6 +1,11 @@
 package controller;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
@@ -22,15 +27,29 @@ public class WSEndpoint {
 
 	private static final Logger LOG = Logger.getLogger(WSEndpoint.class.getName());
 	private static Set<Session> peers = Collections.synchronizedSet(new HashSet<Session>());
-
+	private boolean delete = true;
+	private int i = 2;
+	private HttpURLConnection conn;
+	
 	public WSEndpoint() {
 	}
 
 	@OnMessage
 	public String onMessage(Session session, String message) {
-		
-		publish(message);
-		
+		System.out.println(message);
+
+		if (message.equals("c00d3"))
+			for (Session session2 : peers) {
+				// if(!session2.getId().equals(session.getId())){
+				RemoteEndpoint.Basic other = session2.getBasicRemote();
+				try {
+					other.sendText(message);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				// }
+			}
+
 		return "";
 	}
 
@@ -45,22 +64,59 @@ public class WSEndpoint {
 	public void onClose(Session peer) {
 		System.out.println("CLOSE");
 		LOG.info("Connection closed ...");
-		peers.remove(peer);
-	}
-	
-	public void publish(String message){
-		if (message != null) {
 
-			//Slanje poruka svima osim sebi
-			for (Session s : peers) {
-				RemoteEndpoint.Basic other = s.getBasicRemote();
+		if (delete && i>0) {
+			try {
+
+				URL url1 = new URL("http://localhost:8080/ChatApp/rest/agents/deleteMe/8100");
+				URL url2 = new URL("http://localhost:8080/ChatApp/rest/agents/deleteMe/8090");
+				
+				if(i==2){
+					conn = (HttpURLConnection) url1.openConnection();
+					i--;
+				}else{
+					conn = (HttpURLConnection) url2.openConnection();
+					i--;
+				}
+				
+				conn.setRequestMethod("GET");
+				conn.setRequestProperty("Accept", "application/json");
+
+				BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+				String output;
+				System.out.println("Output from Server .... \n");
+				while ((output = br.readLine()) != null) {
+					System.out.println(output);
+
+				}
+
+				conn.disconnect();
+
+			} catch (MalformedURLException e) {
+
+				e.printStackTrace();
+
+			} catch (IOException e) {
+
+				e.printStackTrace();
+
+			}
+
+			peers.remove(peer);
+
+			for (Session session2 : peers) {
+				// if(!session2.getId().equals(session.getId())){
+				RemoteEndpoint.Basic other = session2.getBasicRemote();
 				try {
-					other.sendText(message);
+					other.sendText("c00d3");
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
+
 			}
-		
+
 		}
 	}
+
 }
